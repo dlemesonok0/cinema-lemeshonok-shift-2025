1a1872e4136479c73ceeec6abc90002f45bf4098
dmitryLem <dlemesonok0@gmail.com>
2025-07-05 13:53:44 +0700

    feat theme switcher

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,59 @@
 import type { NextConfig } from "next";
+import type { RuleSetRule as ModuleRule } from 'webpack';
+
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    webpack(config) {
+        const fileLoaderRule: ModuleRule | undefined = config.module.rules.find((rule: ModuleRule): rule is ModuleRule =>
+            rule.test instanceof RegExp && rule.test.test('.svg')
+        );
+
+        if (fileLoaderRule) {
+            fileLoaderRule.exclude = /\.svg$/;
+        }
+
+        config.module.rules.push({
+            test: /\.svg$/,
+            use: ['@svgr/webpack'],
+        });
+
+        return config;
+    },
 };
+
+module.exports = {
+    turbopack: {
+        rules: {
+            '*.svg': {
+                loaders: [
+                    {
+                        loader: '@svgr/webpack',
+                        options: {
+                            icon: true,
+                            svgoConfig: {
+                                plugins: [
+                                    {
+                                        name: 'convertColors',
+                                        params: { currentColor: true }
+                                    }
+                                ]
+                            }
+                        }
+                    },
+                ],
+                as: '*.js',
+            },
+        },
+    },
+
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'shift-intensive.ru',
+            },
+        ],
+    },
+}
 
 export default nextConfig;
